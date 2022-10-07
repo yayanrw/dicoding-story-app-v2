c86b0e78b015947cf0a835d489d3bf9246ac79eb
yayanrw <yayanraw@gmail.com>
2022-10-07 16:39:10 +0700

    storydto to storyentity

--- app/src/main/java/com/heyproject/storyapp/domain/model/Story.kt
+++ app/src/main/java/com/heyproject/storyapp/domain/model/Story.kt
@@ -1,6 +1,7 @@
 package com.heyproject.storyapp.domain.model
 
-import com.squareup.moshi.Json
+import com.heyproject.storyapp.data.local.entity.StoryEntity
+import com.heyproject.storyapp.data.remote.dto.StoryDto
 
 /**
 Written by Yayan Rahmat Wijaya on 10/7/2022 14:59
@@ -8,11 +9,23 @@ Github : https://github.com/yayanrw
  **/
 
 data class Story(
-    val photoUrl: String?,
-    val createdAt: String?,
-    val name: String?,
-    val description: String?,
+    val photoUrl: String,
+    val createdAt: String,
+    val name: String,
+    val description: String,
     val lon: Double?,
-    val id: String?,
+    val id: String,
     val lat: Double?
 )
+
+fun StoryDto.toEntity(): StoryEntity {
+    return StoryEntity(
+        photoUrl = photoUrl,
+        createdAt = createdAt,
+        name = name,
+        description = description,
+        lon = lon,
+        id = id,
+        lat = lat
+    )
+}
