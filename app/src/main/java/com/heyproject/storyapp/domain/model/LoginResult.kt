58bdce6035c26859af747eb3c1e5117497302bbc
yayanrw <yayanraw@gmail.com>
2022-10-12 15:55:57 +0700

    add LoginResult.kt

---
+++ app/src/main/java/com/heyproject/storyapp/domain/model/LoginResult.kt
@@ -0,0 +1,29 @@
+package com.heyproject.storyapp.domain.model
+
+import com.heyproject.storyapp.data.datasource.remote.dto.LoginResultDto
+
+/**
+Written by Yayan Rahmat Wijaya on 10/12/2022 15:37
+Github : https://github.com/yayanrw
+ **/
+
+data class LoginResult(
+    val name: String, val userId: String, val token: String
+)
+
+fun LoginResultDto.toDomain(): LoginResult {
+    return LoginResult(
+        name = name.orEmpty(),
+        userId = userId.orEmpty(),
+        token = token.orEmpty(),
+    )
+}
+
+fun LoginResult.toLoggedInUser(): User {
+    return User(
+        userId = userId,
+        name = name,
+        token = token,
+        isLogin = true
+    )
+}
