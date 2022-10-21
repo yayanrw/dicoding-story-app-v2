40c6c0cc7a093c744bcc0c7176d9e2e9587c794e
yayanrw <yayanraw@gmail.com>
2022-10-21 16:35:44 +0700

    data dummy stories with location

--- app/src/test/java/com/heyproject/storyapp/utils/DataDummy.kt
+++ app/src/test/java/com/heyproject/storyapp/utils/DataDummy.kt
@@ -3,6 +3,7 @@ package com.heyproject.storyapp.utils
 import com.heyproject.storyapp.data.datasource.remote.dto.LoginResultDto
 import com.heyproject.storyapp.data.datasource.remote.response.GeneralResponse
 import com.heyproject.storyapp.data.datasource.remote.response.LoginResponse
+import com.heyproject.storyapp.domain.model.Story
 
 /**
 Written by Yayan Rahmat Wijaya on 10/21/2022 05:05
@@ -26,5 +27,22 @@ object DataDummy {
 
     fun generateDummyRegisterResponse(): GeneralResponse {
         return GeneralResponse(error = false, message = "success")
+    }
+
+    fun generateDummyStoriesWithLocation(): List<Story> {
+        val stories = ArrayList<Story>()
+        for (i in 1..10) {
+            val story = Story(
+                id = "story-FvU4u0Vp2S3PMsFg",
+                photoUrl = "https://story-api.dicoding.dev/images/stories/photos-1641623658595_dummy-pic.png",
+                createdAt = "2022-01-08T06:34:18.598Z",
+                name = "Yayan",
+                description = "Lorem Ipsum",
+                lon = -16.002,
+                lat = -10.212
+            )
+            stories.add(story)
+        }
+        return stories
     }
 }
