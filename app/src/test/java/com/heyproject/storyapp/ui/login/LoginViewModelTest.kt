0fbe1fb00ab078738b49b91bd09fc0f8ea02140c
yayanrw <yayanraw@gmail.com>
2022-10-21 10:31:44 +0700

    login success test case

---
+++ app/src/test/java/com/heyproject/storyapp/ui/login/LoginViewModelTest.kt
@@ -0,0 +1,64 @@
+package com.heyproject.storyapp.ui.login
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Observer
+import com.heyproject.storyapp.data.repository.UserRepository
+import com.heyproject.storyapp.domain.model.LoginResult
+import com.heyproject.storyapp.domain.model.toDomain
+import com.heyproject.storyapp.util.Result
+import com.heyproject.storyapp.utils.DataDummy
+import org.junit.Assert
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.Mock
+import org.mockito.Mockito
+import org.mockito.Mockito.`when`
+import org.mockito.junit.MockitoJUnitRunner
+
+/**
+ * Written by Yayan Rahmat Wijaya on 10/21/2022 05:02
+ * Github : https://github.com/yayanrw
+ */
+
+@RunWith(MockitoJUnitRunner::class)
+class LoginViewModelTest {
+
+    @get:Rule
+    val instantExecutorRule = InstantTaskExecutorRule()
+
+    @Mock
+    private lateinit var userRepository: UserRepository
+    private lateinit var loginViewModel: LoginViewModel
+    private val dummyLoginResponse = DataDummy.generateDummyLoginResponse()
+
+    private val dummyEmail = "email@mail.com"
+    private val dummyPassword = "password"
+
+    @Before
+    fun setUp() {
+        loginViewModel = LoginViewModel(userRepository)
+    }
+
+    @Test
+    fun `Login Success with result success`() {
+        val observer = Observer<Result<LoginResult>> {}
+
+        try {
+            val expectedResponse = MutableLiveData<Result<LoginResult>>()
+            expectedResponse.value = Result.Success(dummyLoginResponse.loginResult!!.toDomain())
+
+            `when`(userRepository.logIn(dummyEmail, dummyPassword)).thenReturn(expectedResponse)
+            val actualResponse =
+                loginViewModel.signIn(dummyEmail, dummyPassword).observeForever(observer)
+
+            Mockito.verify(userRepository).logIn(dummyEmail, dummyPassword)
+            Assert.assertNotNull(actualResponse)
+        } finally {
+            loginViewModel.signIn(dummyEmail, dummyPassword).removeObserver(observer)
+        }
+
+    }
+}
