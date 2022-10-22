db935952bb718adc900150c3ad074d85e813fd55
yayanrw <yayanraw@gmail.com>
2022-10-22 11:27:23 +0700

    add MainDispatcherRule.kt

---
+++ app/src/test/java/com/heyproject/storyapp/utils/MainDispatcherRule.kt
@@ -0,0 +1,28 @@
+package com.heyproject.storyapp.utils
+
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.TestDispatcher
+import kotlinx.coroutines.test.UnconfinedTestDispatcher
+import kotlinx.coroutines.test.resetMain
+import kotlinx.coroutines.test.setMain
+import org.junit.rules.TestWatcher
+import org.junit.runner.Description
+
+/**
+Written by Yayan Rahmat Wijaya on 10/22/2022 11:01
+Github : https://github.com/yayanrw
+ **/
+
+@ExperimentalCoroutinesApi
+class MainDispatcherRule(
+    private val testDispatcher: TestDispatcher = UnconfinedTestDispatcher()
+) : TestWatcher() {
+    override fun starting(description: Description) {
+        Dispatchers.setMain(testDispatcher)
+    }
+
+    override fun finished(description: Description) {
+        Dispatchers.resetMain()
+    }
+}
